a6f320de24ae275071195762de326a3280140e44
Carlos <jc_solos@hotmail.com>
2024-03-14 04:37:25 -0300

    primeira mudança

---
+++ modelo.py
@@ -0,0 +1,70 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_like(self):
+        self._likes = self._likes + 1
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    def __str__(self):
+        return f"{self._nome} - {self.ano} - {self._likes} likes"
+
+
+class Filme(Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+    def __str__(self):
+        return f"{self._nome} - {self.ano} - {self.duracao} min - {self._likes} likes"
+
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
+
+    def __str__(self):
+        return f"{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} likes"
+
+
+class Playlist(list):
+    def __init__(self, nome, programas):
+        self.nome = nome
+        super().__init__(programas)
+
+
+vingadores = Filme("vingadores-guerra infinita", 2018, 160)
+atlanta = Serie("atlanta", 2018, 2)
+tmep = Filme("todo mundo em pânico", 1999, 100)
+demolidor = Serie("demolidor", 2016, 2)
+
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
+
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+playlist_fim_de_semana = Playlist("fim de semana", filmes_e_series)
+
+for programa in playlist_fim_de_semana:
+    print(programa)
