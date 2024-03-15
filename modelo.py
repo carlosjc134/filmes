03bac9ee418e3e5dcd80c298bb22703f9f8cfa49
Carlos <jc_solos@hotmail.com>
2024-03-15 04:44:49 -0300

    mudando

--- modelo.py
+++ modelo.py
@@ -46,14 +46,15 @@ class Playlist:
         self.nome = nome
         self._programas = programas
 
+    def __getitem__(self, item):
+        return self._programas[item]
+
     @property
     def listagem(self):
         return self._programas
 
-    @property
-    def tamanho(self):
+    def __len__(self):
         return len(self._programas)
-
 
 
 vingadores = Filme("vingadores-guerra infinita", 2018, 160)
@@ -75,11 +76,11 @@ atlanta.dar_like()
 filmes_e_series = [vingadores, atlanta, demolidor, tmep]
 playlist_fim_de_semana = Playlist("fim de semana", filmes_e_series)
 
-print(f"Tamanho do playlist: {len(playlist_fim_de_semana.listagem)}")
+print(f"Tamanho do playlist: {len(playlist_fim_de_semana)}")
+
+print(vingadores in playlist_fim_de_semana)
+
+len(playlist_fim_de_semana)
 
 for programa in playlist_fim_de_semana.listagem:
     print(programa)
-
-
-
-
